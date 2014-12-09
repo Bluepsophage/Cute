82b74cf9397322b71c7365baa4f135447d9e5f6d
bluepsophage <mathilde.husson@supinternet.fr>
2014-12-09 11:25:18 +0100

    First commit to create a Git repository from existing sources

---
+++ app/controllers/index.js
@@ -0,0 +1,72 @@
+function doClick(e) {
+    //When the source element (the mouth) is clicked, the CSS class is changed, so it looks like a duckface
+    $.addClass(e.source, "mouthDuckface");
+    //The regular mouth class is removed
+    $.removeClass(e.source, "mouth");
+
+    //Then the camera is displayed
+    Titanium.Media.showCamera({
+        //If a picture is captured correctly
+        success:function(event) {
+            //If it is really a picture
+            if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+                var imageView = Ti.UI.createImageView({
+                    image:event.media
+                });
+            }
+            else {
+                alert("Got the wrong type back :"+event.mediaType);
+            }
+            //Finally we put the regular mouth back
+            $.removeClass(e.source, "mouthDuckface");
+            $.addClass(e.source, "mouth");
+        },
+        //If the user choose to cancel the picture
+        cancel:function() {
+            $.removeClass(e.source, "mouthDuckface");
+            $.addClass(e.source, "mouth");
+            return;
+        },
+        //If an error occurs
+        error:function(error) {
+            var a = Titanium.UI.createAlertDialog({title:'Camera'});
+            //If there is no camera
+            if (error.code == Titanium.Media.NO_CAMERA) {
+                a.setMessage('Please run this test on device');
+            }
+            else {
+                a.setMessage('Unexpected error : ' + error.code);
+            }
+            a.show();
+        },
+        //Save the picture in the Gallery, in a custom folder
+        saveToPhotoGallery:true,
+    });
+}
+
+//Will execute when the device is shaken
+Titanium.Gesture.addEventListener('shake', function(e) {
+    //Access element by id to change the classes
+    $.addClass($.mouth, "mouthPanic");
+    $.removeClass($.mouth, "mouth");
+
+    $.addClass($.eyeLeft, "eyeLeftPanic");
+    $.removeClass($.eyeLeft, "eyeLeft");
+
+    $.addClass($.eyeRight, "eyeRightPanic");
+    $.removeClass($.eyeRight, "eyeRight");
+
+    //Wait three seconds before putting back the regular face
+    setTimeout(function(){
+        $.addClass($.mouth, "mouth");
+        $.removeClass($.mouth, "mouthPanic");
+
+        $.addClass($.eyeLeft, "eyeLeft");
+        $.removeClass($.eyeLeft, "eyeLeftPanic");
+
+        $.addClass($.eyeRight, "eyeRight");
+        $.removeClass($.eyeRight, "eyeRightPanic");
+    }, 3000);
+});
+
+$.index.open();
